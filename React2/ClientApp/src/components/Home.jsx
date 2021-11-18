a90eb0eca1c5c22a524a96b85246578ba04d5594
Dmitrii Kotov <dkotov@outlook.co.nz>
2021-11-18 18:22:04 +1300

    Testing

--- React2/ClientApp/src/components/Home.jsx
+++ React2/ClientApp/src/components/Home.jsx
@@ -11,7 +11,7 @@ export class Home extends Component {
         <ul>
           <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
           <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
-          <li><a href='https://react.semantic-ui.com/'>Sematic UI +++++++</a> for layout and styling</li>
+          <li><a href='https://react.semantic-ui.com/'>Sematic UI</a> for layout and styling</li>
         </ul>
       </div>
     );
