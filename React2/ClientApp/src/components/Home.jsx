7034c01c5d561a707721041df5fc5de6da3d8b47
Dmitrii Kotov <dkotov@outlook.co.nz>
2021-11-22 21:25:59 +1300

    Updated:
    - Home PAge with Task and Source COde info

--- React2/ClientApp/src/components/Home.jsx
+++ React2/ClientApp/src/components/Home.jsx
@@ -3,13 +3,15 @@ import React, { Component } from 'react';
 export class Home extends Component {
   static displayName = Home.name;
 
-  render () {
+  render() {
     return (
       <div>
-            <h1>First App With React</h1>
-            <p>22/11/2021 Update: Project is under maintenance. Migrating SQL sever to Azure</p>
+        <h1>First App With React</h1>
+        <p>22/11/2021 Update: Project is under maintenance. Migrating SQL sever to Azure</p>
         <p> built by Dmitrii Kotov with:</p>
         <ul>
+          <li><a href='https://docs.google.com/document/d/1u3Sm2yPAmOs3NnjpsWq92TO20-bgoGZTYk-Khhom7Cw/edit'> Task Link</a></li>
+          <li><a href='https://github.com/KotovD/React2'> Source Code on GitHub</a></li>
           <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
           <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
           <li><a href='https://react.semantic-ui.com/'>Sematic UI</a> for layout and styling</li>
